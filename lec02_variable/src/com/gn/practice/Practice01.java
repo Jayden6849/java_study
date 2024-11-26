e8c6835e3e76defb409c3a8ce2ce6e10808e44f1
Jayden6849 <jayden6849@gmail.com>
2024-11-26 16:14:47 +0900

    [Jayden] commit for lec02_variable
    	- add VariableBasic.java
    	- add Practice01.java

---
+++ lec02_variable/src/com/gn/practice/Practice01.java
@@ -0,0 +1,12 @@
+package com.gn.practice;
+
+public class Practice01 {
+	public static void main(String[] args) {
+		int num1 = 100;
+		int num2 = 10;
+		System.out.println(num1 + num2); // 110
+		System.out.println(num2 * (num1 + num2)); // 1100
+		System.out.println(num2 * (num1 + num2) / num2); // 110
+		
+	}
+}
--- lec02_variable/src/com/gn/study/VariableBasic.java
+++ lec02_variable/src/com/gn/study/VariableBasic.java
@@ -2,6 +2,27 @@ package com.gn.study;
 
 public class VariableBasic {
 	public static void main(String[] args) {
+		// 1. 변수 선언
+		int number;
+		
+		// 2. 변수의 초기화(initiate)
+		number = 123;
+
+		// 3. 선언과 초기화를 동시에
+		int age = 10;
+//		int age = 40; // 선언은 1번만 가능함
+		int age2 = 12;
+		
+		// 4. 변수를 표준출력 해보기
+		System.out.println(number);
+		System.out.println(age);
+		System.out.println(age2);
+		
+		// 5. 재할당(reallocate)
+		int score = 90;
+		System.out.println("1 : " + score);
+		score = 95;
+		System.out.println("2 : " + score);
 		
 	}
 }
