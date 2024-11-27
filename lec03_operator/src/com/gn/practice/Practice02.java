6a21c06c1c989d88006bc69c58fc04fe82834995
Jayden6849 <jayden6849@gmail.com>
2024-11-27 16:08:38 +0900

    [Jayden] Update lec03_operator
    	- edit ArithmeticOperator.java
    	- edit Practice02.java

--- lec03_operator/src/com/gn/practice/Practice02.java
+++ lec03_operator/src/com/gn/practice/Practice02.java
@@ -3,6 +3,7 @@ package com.gn.practice;
 public class Practice02 {
 	public static void main(String[] args) {
 		int result = 100;
+		
 		result += 3;
 		System.out.println(result); //103
 		result -= 10;
--- lec03_operator/src/com/gn/study/ArithmeticOperator.java
+++ lec03_operator/src/com/gn/study/ArithmeticOperator.java
@@ -34,5 +34,28 @@ public class ArithmeticOperator {
 		int d = 10;
 		d %= 3;
 		System.out.println("d = " + d); // 1
+		
+		// 홀수와 짝수를 나누는 메커니즘
+		// 2로 나누어 나머지가 1이면 홀수, 0이면 짝수
+		
+		if(a%2 != 0) {
+			System.out.println("홀수");
+		} else {
+			System.out.println("짝수");
+		}
+		
+		// 3. 증감연산자 ++ --
+		int pre = 5;
+		System.out.println(pre); // 5
+		System.out.println(++pre); // 6 :: 더해진 후 출력됨 (해당 라인에서 연산)
+		System.out.println(pre); // 6
+		
+		int post = 5;
+		System.out.println(post); // 5
+		System.out.println(post++); // 5 :: 출력된 후 더해짐 (다음 라인에서 연산)
+		System.out.println(post); // 6
+		
+		
+		
 	}
 }
