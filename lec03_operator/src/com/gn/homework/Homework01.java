835e279d57f5ce4796f92026e58663ad1043e608
Jayden6849 <jayden6849@gmail.com>
2024-11-28 17:55:29 +0900

    [Jayden] Edit Homework01
    	- edit Homework01

--- lec03_operator/src/com/gn/homework/Homework01.java
+++ lec03_operator/src/com/gn/homework/Homework01.java
@@ -7,14 +7,8 @@ public class Homework01 {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("말해보세요 : ");
-		String input = sc.nextLine();
-		String output = "";
 		
-		output = input.equals("간다") ? "온다" : "입력오류";
-		output = "온다".equals(input) ? "간다" : "입력오류";
-
-		
-		System.out.println(output);
+		String input = sc.next();
 		
 		sc.close();
 	}
