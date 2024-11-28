b35d3fbb268339c2536a3c0eb1c83a2f82f0488a
Jayden <jayden6849@gmail.com>
2024-11-28 21:27:24 +0900

    [Jayden] Edit Homework01 (lec03)
    	- edit Homework01

--- lec03_operator/src/com/gn/homework/Homework01.java
+++ lec03_operator/src/com/gn/homework/Homework01.java
@@ -10,6 +10,15 @@ public class Homework01 {
 		
 		String input = sc.next();
 		
+		String go = "간다";
+		String come = "온다";
+		
+		if(go.equals(input)) {
+			System.out.println("앵무새 : " + come);
+		} else if(come.equals(input)) {
+			System.out.println("앵무새 : " + go);
+		}
+		
 		sc.close();
 	}
 }
