3588071c111f0d98ed3bcb7f67d297030a75477e
Jayden6849 <jayden6849@gmail.com>
2024-11-29 11:14:35 +0900

    [Jayden] edit com.gn.homework.test (lec04)
    	- edit Practice
    	- edit Run

--- lec04_control/src/com/gn/homework/test/Practice.java
+++ lec04_control/src/com/gn/homework/test/Practice.java
@@ -1,5 +1,23 @@
 package com.gn.homework.test;
 
-public class Practice {
+import java.util.Scanner;
 
+public class Practice {
+	// Method
+	// 1. 인사말을 출력하는 기능(메소드)
+	public void greet() {
+		System.out.println("안녕하세요~");
+	}
+	
+	// 2. 두개의 숫자를 입력 받아서 합을 출력하는 기능
+	public void scanAdd() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("정수 2개를 입력해주세요 : ");
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		
+		System.out.println(num1+num2);
+		
+		sc.close();
+	}
 }
--- lec04_control/src/com/gn/homework/test/Run.java
+++ lec04_control/src/com/gn/homework/test/Run.java
@@ -2,6 +2,10 @@ package com.gn.homework.test;
 
 public class Run {
 	public static void main(String[] args) {
+		// Practice 가 public 이어서 별다른 절차없이 가져옴
+		Practice p = new Practice();
 		
+		p.greet();
+		p.scanAdd();
 	}
 }
