e373d24965605e95c212384576b8ae6906d1323e
Jayden6849 <jayden6849@gmail.com>
2024-11-29 11:50:57 +0900

    [Jayden] edit Practice05 (lec04)
    	- edit Practice05

---
+++ lec04_control/src/com/gn/study/LoopStmt.java
@@ -0,0 +1,13 @@
+package com.gn.study;
+
+public class LoopStmt {
+	public static void main(String[] args) {
+		// 1-10까지 모든 정수를 출력하고 싶을 경우
+		for(int i=1; i<=10; i++) {
+			System.out.println(i);
+		}
+//		System.out.println(i); // int i 가 선언된 scope를 벗어남. 쓰고싶다면 for문 밖에서 선언하면 됌
+		
+		
+	}
+}
