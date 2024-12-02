349c76974c25cbb4968f4adfc0098dce914124e7
Jayden6849 <jayden6849@gmail.com>
2024-12-02 17:34:26 +0900

    [Jayden] Finish com.gn.homework.loop (lec04)
    	- add practice01()
    	- add practice04()
    	- add practice05()
    	- add practice08()

--- lec04_control/src/com/gn/homework/loop/Practice.java
+++ lec04_control/src/com/gn/homework/loop/Practice.java
@@ -3,6 +3,29 @@ package com.gn.homework.loop;
 import java.util.Scanner;
 
 public class Practice {
+	public void practice01() {
+		Scanner sc = new Scanner(System.in);
+		
+		int sum = 0;
+		while(true) {
+			System.out.print("정수(1~50) : ");
+			int inputNum = sc.nextInt();
+			
+			if(inputNum < 1 || 50 < inputNum) {
+				System.out.println("1~50 사이의 정수를 입력하세요.");
+				continue;
+			} else {
+				for(int i=1; i<=inputNum; i++) {
+					if(i%2==0) sum += i;
+				}
+				break;
+			}
+		}
+		System.out.println("합계 : " + sum);
+		
+		sc.close();
+	}
+	
 	public void practice02() {
 		Scanner sc = new Scanner(System.in);
 		
@@ -47,6 +70,47 @@ public class Practice {
 		}
 	}
 	
+	public void practice04() {
+		Scanner sc = new Scanner(System.in);
+		
+		do {
+			System.out.println("게임을 실행합니다!");
+			
+			System.out.println("게임을 다시 플레이하시겠습니까?(yes/no)");
+			String input = sc.next();
+			
+			if("yes".equalsIgnoreCase(input)) {
+				continue;
+			} else if("no".equalsIgnoreCase(input)) {
+				System.out.println("게임을 종료합니다.");
+				sc.close();
+				break;
+			}			
+		} while(true);
+	}
+	
+	public void practice05() {
+		Scanner sc = new Scanner(System.in);
+
+		int sum = 0;
+		
+		while(true) {
+			System.out.print("숫자(0을 입력하면 종료) : ");
+			int inputNum = sc.nextInt();
+			
+			if(inputNum == 0) {
+				break;
+			} else if(inputNum < 0) {
+				continue;	
+			} else {
+				sum += inputNum;				
+			}
+		}
+		
+		System.out.println("양수의 합계 : " + sum);
+		sc.close();
+	}
+	
 	public void practice06() {
 		String tree = "";
 		
@@ -80,5 +144,40 @@ public class Practice {
 		}
 		
 		sc.close();
+	}
+	
+	public void practice08() {
+		Scanner sc = new Scanner(System.in);
+		
+		exter:
+		do {
+			System.out.print("문자열 : ");
+			String inputStr = sc.next();
+			System.out.print("문자 : ");
+			char inputChar = sc.next().charAt(0);
+			
+			int sum = 0;
+			char[] chArr = inputStr.toCharArray();
+			
+			for(int i=0; i<chArr.length; i++) {
+				if(chArr[i] == inputChar) sum += 1;
+			}
+			System.out.println("포함된 개수 : " + sum);
+		
+			do {
+				System.out.print("다시 입력하시겠습니까? : ");
+				String answer = sc.next();
+				
+				if("y".equalsIgnoreCase(answer)) {
+					continue exter;
+				} else if("n".equalsIgnoreCase(answer)) {
+					sc.close();
+					break exter;
+				} else {
+					System.out.println("잘못된 대답입니다. 다시 입력해주세요.");
+					continue;
+				}
+			} while(true);
+		} while(true);
 	}
 }
--- lec04_control/src/com/gn/homework/loop/Run.java
+++ lec04_control/src/com/gn/homework/loop/Run.java
@@ -8,10 +8,14 @@ public class Run {
 		
 		System.out.println("실행할 기능을 입력하세요.");
 		
+		System.out.println("1. 1~50까지 짝수 합하기");
 		System.out.println("2. 구구단 무한반복");
 		System.out.println("3. 탈출합시다.");
+		System.out.println("4. 게임 재시작");
+		System.out.println("5. 양수만 더하기");
 		System.out.println("6. 트리 만들기");
 		System.out.println("7. 박수박수박");
+		System.out.println("8. 문자열의 개수");
 		
 		System.out.print("선택: ");
 		int menu = sc.nextInt();
@@ -19,17 +23,29 @@ public class Run {
 		Practice p = new Practice();
 		
 		switch (menu) {
+		case 1:
+			p.practice01();
+			break;
 		case 2:
 			p.practice02();
 			break;
 		case 3:
 			p.practice03();
 			break;
+		case 4:
+			p.practice04();
+			break;
+		case 5:
+			p.practice05();
+			break;
 		case 6:
 			p.practice06();
 			break;
 		case 7:
 			p.practice07();
+			break;
+		case 8:
+			p.practice08();
 			break;
 		}
 		
