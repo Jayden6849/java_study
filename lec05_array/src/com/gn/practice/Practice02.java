977064e7f8b76be5c257e56b83727acfee225624
Jayden6849 <jayden6849@gmail.com>
2024-12-02 14:36:07 +0900

    [Jayden] Modify Practice02 (lec05)
    	- modify Practice02

--- lec05_array/src/com/gn/practice/Practice02.java
+++ lec05_array/src/com/gn/practice/Practice02.java
@@ -1,11 +1,33 @@
 package com.gn.practice;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Practice02 {
 	public static void main(String[] args) {
-		int[] iArr = {-62, 107, -12, 89, -35};
+//		int[] iArr = {-62, 107, -12, 89, -35};
+//		
+//		for(int num : iArr) {
+//			if(num > 50) System.out.println(num);
+//		}
+		Scanner sc = new Scanner(System.in);
 		
-		for(int num : iArr) {
+		int[] intArr = new int[5];
+			
+		try {
+			System.out.print("임의의 정수 5개를 입력해주세요 : ");
+			for(int i=0; i<intArr.length; i++) {
+				intArr[i] = sc.nextInt();
+			}
+		} catch (InputMismatchException ime) {
+			sc.next();
+			System.out.println("주의 :: 정수를 입력해주셔야합니다.");
+		}
+		
+		for(int num : intArr) {
 			if(num > 50) System.out.println(num);
 		}
+		
+		sc.close();
 	}
 }
