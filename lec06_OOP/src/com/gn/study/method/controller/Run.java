0e3cf89e59cd0365f7d7f7a71c368a5ca4e0fbfb
Jayden6849 <jayden6849@gmail.com>
2024-12-04 11:53:56 +0900

    [Jayden] Add pkg_practice03 (lec06)
    	- add Calculator
    	- add Smartphone
    	- add Run

---
+++ lec06_OOP/src/com/gn/study/method/controller/Run.java
@@ -0,0 +1,11 @@
+package com.gn.study.method.controller;
+
+import com.gn.study.method.model.vo.Smartphone;
+
+public class Run {
+	public static void main(String[] args) {
+		Smartphone p = new Smartphone();
+		
+		
+	}
+}
---
+++ lec06_OOP/src/com/gn/study/method/model/vo/Smartphone.java
@@ -0,0 +1,32 @@
+package com.gn.study.method.model.vo;
+
+public class Smartphone {
+	private int battery;
+	private int volume;
+	
+	// 1. 반환형x, 매개변수x :: 앱 알림 메시지
+	public void openApp() {
+		System.out.println("앱이 열렸습니다.");
+	}
+	// 2. 반환형o, 매개변수x :: 배터리 잔량 반환
+	public int checkBattery() {
+		return battery;
+	}
+	// 3. 반환형x, 매개변수o :: 볼륨값을 입력하면 현재볼륨을 출력
+	public void setVolumn(int volume) {
+		this.setVolume(volume);
+		System.out.println("볼륨이 " + volume + "%으로 설정되었습니다.");
+	}
+	// 4. 반환형o, 매개변수o :: 메시지 전송
+	public String sendMsg(String msg) {
+		return "메시지 \'" + msg + "\'가 전송되었습니다."; 
+	}
+	
+	// getter, setter
+	public int getVolume() {
+		return volume;
+	}
+	public void setVolume(int volume) {
+		this.volume = volume;
+	}
+}
