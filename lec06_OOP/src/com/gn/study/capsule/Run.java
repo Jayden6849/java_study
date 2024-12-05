97465cf45a8488f1f70858123c4bf9063b7923e6
Jayden6849 <jayden6849@gmail.com>
2024-12-05 12:15:22 +0900

    [Jayden] edit Account2 (lec06)
    	- edit Account2

--- lec06_OOP/src/com/gn/study/capsule/Run.java
+++ lec06_OOP/src/com/gn/study/capsule/Run.java
@@ -1,5 +1,6 @@
 package com.gn.study.capsule;
 
+import com.gn.study.capsule.closed.Account2;
 import com.gn.study.capsule.open.Account1;
 
 public class Run {
@@ -13,9 +14,21 @@ public class Run {
 		int id = s.getId();
 		System.out.println(id);
 		
+		// 캡슐화가 이루어지지 않은 케이스
+		System.out.println("=== uncapsuled ===");
 		Account1 a1 = new Account1("김철수", 20000000);
 		a1.balance -= 50000000; // 잔액이 모자람에도 출금이 발생함 :: 필드의 무결성이 보장되지 못함.
 		
 		System.out.println(a1.balance);
+		
+		// 캡슐화가 이루어진 케이스
+		System.out.println("=== capsuled ===");
+		Account2 a2 = new Account2("김철수", 20000000);
+//		a2.balance -= 50000000; // 캡슐화되어 있기때문에 필드에 직접 접근하는게 불가능함
+		System.out.println(a2.getBalance());
+		a2.deposit(10000000);
+		System.out.println(a2.getBalance());
+		a2.withdraw(50000000);
+		System.out.println(a2.getBalance());
 	}
 }
--- lec06_OOP/src/com/gn/study/capsule/closed/Account2.java
+++ lec06_OOP/src/com/gn/study/capsule/closed/Account2.java
@@ -1,5 +1,40 @@
 package com.gn.study.capsule.closed;
 
-public class Account2 {
+/**
+ * 캡슐화가 제대로 되어있는 케이스
+ */
 
+public class Account2 {
+	private String user;
+	private int balance;
+	
+	// constructor
+	public Account2() {
+		this("홍길동", 0);
+	}
+	public Account2(String user, int balance) {
+		this.user = user;
+		this.balance = balance;
+	}
+	
+	// getter, setter
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public int getBalance() {
+		return balance;
+	}
+	public void deposit(int money) {
+		this.balance += money;
+	}
+	
+	public void withdraw(int money) {
+		if(this.balance-money >= 0) this.balance -= money;
+		else System.out.println("잔액이 부족합니다.");
+	}
+	
+	
 }
