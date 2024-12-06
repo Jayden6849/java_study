f2ca656d2f150f415d4ad07b7a31e7cda56ee67a
Jayden6849 <jayden6849@gmail.com>
2024-12-06 11:07:28 +0900

    [Jayden] add pkg_practice01 (lec08)
    	- add Employee
    	- add Manager
    	- add Run

---
+++ lec08_inheritance/src/com/gn/practice01/model/vo/Employee.java
@@ -0,0 +1,24 @@
+package com.gn.practice01.model.vo;
+
+public class Employee {
+	protected String name;
+	
+	public Employee() {
+		super();
+	}
+	public Employee(String name) {
+		super();
+		this.name = name;
+	}
+	
+	public void printInfo() {
+		System.out.println("직원 이름 : " + name);
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ lec08_inheritance/src/com/gn/practice01/model/vo/Manager.java
@@ -0,0 +1,24 @@
+package com.gn.practice01.model.vo;
+
+public class Manager extends Employee {
+	private String department;
+	
+	public Manager() {
+		super();
+	}
+	public Manager(String name, String department) {
+		super(name);
+		this.department = department;
+	}
+	
+	public void printDepartment() {
+		System.out.println("담당 부서 : " + department);
+	}
+	
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+}
