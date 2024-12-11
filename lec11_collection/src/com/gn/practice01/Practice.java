c754fe4bb4039450268e96ab420536f61e37db6a
Jayden6849 <jayden6849@gmail.com>
2024-12-11 11:35:27 +0900

    [Jayden] add pkg_practice02 (lec11)
    	- redirect pkg_practice01
    	- add Run, Pair

---
+++ lec11_collection/src/com/gn/practice01/Practice.java
@@ -0,0 +1,8 @@
+package com.gn.practice01;
+
+public class Practice {
+	public void printSum(String a, String b) {
+		System.out.print("합 : ");
+		System.out.println(Integer.parseInt(a) + Integer.parseInt(b));
+	}
+}
---
+++ lec11_collection/src/com/gn/practice02/Run.java
@@ -0,0 +1,10 @@
+package com.gn.practice02;
+
+public class Run {
+	public static void main(String[] args) {
+		Pair<String, Integer> pair = new Pair<>("Apple", 10);
+		
+		System.out.println("First : " + pair.getFirst());
+		System.out.println("Second : " + pair.getSecond());
+	}
+}
