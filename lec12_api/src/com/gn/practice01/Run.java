6906965458e31667f44b6892915c18d5ec73dc7a
Jayden6849 <jayden6849@gmail.com>
2024-12-16 10:09:25 +0900

    [Jayden] add pkg_study, pkg_practice01 (lec12)
    	- add pkg_study
    	- add pkg_practice01

---
+++ lec12_api/src/com/gn/practice01/Run.java
@@ -0,0 +1,30 @@
+package com.gn.practice01;
+
+public class Run {
+	public static void main(String[] args) {
+		long start = System.currentTimeMillis();
+		
+		int[] src = new int[1000000];
+		
+		for(int i=0; i<src.length; i++) {
+			src[i] = (int)(Math.random()*100+1);
+		}
+		
+		int[] dst = new int[src.length/2];
+		
+		System.arraycopy(src, 0, dst, 0, src.length/2);
+		
+		for(int i=0; i<dst.length; i++) {
+			if(i%10 == 9) {
+				System.out.print(dst[i]);
+				System.out.println();
+			} else {
+				System.out.print(dst[i] + ",");
+			}
+		}
+		
+		long end = System.currentTimeMillis();
+		
+		System.out.println("복사 작업 소요 시간 : " + (end - start) + "ms");
+	}
+}
--- lec12_api/src/com/gn/study/controller/SystemRun.java
+++ lec12_api/src/com/gn/study/controller/SystemRun.java
@@ -1,7 +1,36 @@
 package com.gn.study.controller;
 
+import java.util.Arrays;
+
 public class SystemRun {
 	public static void main(String[] args) {
+		long before = System.currentTimeMillis();
 		
+		// 1. arraycopy()
+		int[] src = {10, 15, 20, 25, 30};
+		
+		int[] dst = new int[src.length];
+		
+//		for(int i=0; i<src.length; i++) {
+//			dst[i] = src[i];
+//		}
+//		
+//		for(int d : dst) {
+//			System.out.print(d + " | ");
+//		}
+//		System.out.println();
+		
+		System.arraycopy(src, 0, dst, 0, src.length);
+		
+		System.out.println(Arrays.toString(src));
+		System.out.println(Arrays.toString(dst));
+		
+		long after = System.currentTimeMillis();
+		System.out.println(after - before);
+		
+		// 2. currentTimeMillis();
+		
+		long timestamp = System.currentTimeMillis();
+		System.out.println(timestamp);
 	}
 }
