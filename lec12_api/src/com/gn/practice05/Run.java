4df7c7e48068c43db16dd80a7cc0f38edaa93506
Jayden6849 <jayden6849@gmail.com>
2024-12-17 09:37:14 +0900

    [Jayden] 반복문으로 리팩토링 (lec12...practice05)
    	- edit Run

--- lec12_api/src/com/gn/practice05/Run.java
+++ lec12_api/src/com/gn/practice05/Run.java
@@ -6,18 +6,24 @@ public class Run {
 		
 		String str1 = "";
 		
-		for(int i=0; i<words.length; i++) {
-			if(i == words.length-1) {
-				str1 += words[i];
-			} else {
-				str1 += words[i] + " ";
-			}
-		}
+//		for(int i=0; i<words.length; i++) {
+//			if(i == words.length-1) {
+//				str1 += words[i];
+//			} else {
+//				str1 += words[i] + " ";
+//			}
+//		}
+		
+		str1 = String.join(" ", words);
+		
 		System.out.println("문장 : " + str1);
 		
 		String str2 = "";
 		
-		str2 = str1.substring(5, 6) + str1.substring(17, 18) + str1.substring(29, 30);
+		for(int i=0; i<words.length; i++) {
+			if(i==0) continue;
+			str2 += words[i].substring(0,1);
+		}
 		
 		System.out.println("변환 : " + str2.toUpperCase());
 	}
