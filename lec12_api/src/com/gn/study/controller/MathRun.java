09f9f55dc3b6894422d65da22adde3455fefcd27
Jayden6849 <jayden6849@gmail.com>
2024-12-17 12:10:53 +0900

    [Jayden] add pkg_practice08 (lec12)
    	- ArrayList + Math.random()

--- lec12_api/src/com/gn/study/controller/MathRun.java
+++ lec12_api/src/com/gn/study/controller/MathRun.java
@@ -17,5 +17,16 @@ public class MathRun {
 		
 		double after = Math.round(PI*100.0)/100.0;
 		System.out.println(after);
+		
+		// 4. ceil
+		double up = Math.ceil(PI);
+		System.out.println(up);
+		
+		// 5. floor
+		double down = Math.floor(PI);
+		System.out.println(down);
+		
+		System.out.println(Math.ceil(PI*Math.pow(10, 2))/Math.pow(10, 2));
+		System.out.println(Math.floor(PI*Math.pow(10, 2))/Math.pow(10, 2));
 	}
 }
